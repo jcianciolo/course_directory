e3173b2db2338e96888c5471693fd251189351dd
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2025-02-13 18:48:09 -0800

    add: courseSection.ts

---
+++ src/app/drizzle/schema/courseSection.ts
@@ -0,0 +1,25 @@
+import { pgEnum, pgTable, text, integer, uuid } from "drizzle-orm/pg-core";
+import { createdAt, id, updatedAt } from "../schemaHelpers";
+import { CourseTable } from "./course";
+import { relations } from "drizzle-orm";
+
+export const courseSectionStatuses = ["public", "private"] as const;
+export type CourseSectionStatus = (typeof courseSectionStatuses)[number];
+export const courseSectionStatusEnum = pgEnum("course_section_status", courseSectionStatuses);
+
+export const CourseSectionTable = pgTable("course_sections", {
+    id,
+    name: text().notNull(),
+    status: courseSectionStatusEnum().notNull().default("private"),
+    order: integer().notNull(),
+    courseId: uuid().notNull().references(() => CourseTable.id, { onDelete: "cascade" }),
+    createdAt,
+    updatedAt,
+});
+
+export const CourseSectionRelationships = relations(CourseSectionTable, ({ many, one }) => ({
+    course: one(CourseTable, {
+        fields: [CourseSectionTable.courseId],
+        references: [CourseTable.id],
+    }),
+}));
